48b44189dd1fba2bf7fdba123dafcfff5b566890
조현승 <danitech171791@gmail.com>
2024-05-24 14:24:07 +0900

    [feat/게임서버] 네트워크 스폰 부가 오브젝트 - 기초구조1
    - 해당 컴포넌트가 사용할 변수 선언
    
    - destroyAfter -> 이벤트가 발생하고 해당 오브젝트가 자동 파괴될 시간
    - force -> 리지드바디에서 AddForce용 값

--- Assets/Mirror/Ref/MirrorGame_NetDoggie/Scripts/NetSpawnedSubObject.cs
+++ Assets/Mirror/Ref/MirrorGame_NetDoggie/Scripts/NetSpawnedSubObject.cs
@@ -1,7 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class NetSpawnedSubObject : MonoBehaviour
+public class NetSpawnedSubObject : NetworkBehaviour
 {
+    public float _destroyAfter = 2.0f;
+    public float _force = 1000;
+
+    public Rigidbody RigidBody_SubObj;
+
 }
