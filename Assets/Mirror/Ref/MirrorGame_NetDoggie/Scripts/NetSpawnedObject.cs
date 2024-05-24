17ec907d66822e2baf1bd40eef23ce92d73b2a55
조현승 <danitech171791@gmail.com>
2024-05-24 09:38:23 +0900

    [Init/게임서버] 네트워크 스폰 오브젝트 - 기초 구조3
    - NetSpawnObject에서 사용할 오브젝트들 선언
    - NetAgent_Player : 플레이어 현재 속도 판단용으로 사용
    - Animator_Player : 플레이어 애니메이션 제어용
    - TextMesh_HealthBar : 플레이어 HP 표기용 텍스트 메쉬
    - Transform_Player : 플레이어 위치, 회전 제어용
    
    - _rotationSpeed = 회전 속도 값
    - _atkKey = 공격 키
    - Prefab_AtkObject = 공격할 때 스폰할 데미지 오브젝트
    - Transform_AtkSpawnPos = 데미지 오브젝트 스폰용 위치
    - _health = 현재 플레이어 체력

--- Assets/Mirror/Ref/MirrorGame_NetDoggie/Scripts/NetSpawnedObject.cs
+++ Assets/Mirror/Ref/MirrorGame_NetDoggie/Scripts/NetSpawnedObject.cs
@@ -2,8 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.AI;
 
 public class NetSpawnedObject : NetworkBehaviour
 {
+    [Header("Components")]
+    public NavMeshAgent NavAgent_Player;
+    public Animator Animator_Player;
+    public TextMesh TextMesh_HealthBar;
+    public Transform Transform_Player;
 
+    [Header("Movement")]
+    public float _rotationSpeed = 100.0f;
+
+    [Header("Attack")]
+    public KeyCode _atkKey = KeyCode.Space;
+    public GameObject Prefab_AtkObject;
+    public Transform Tranfrom_AtkSpawnPos;
+
+    [Header("Stats Server")]
+    [SyncVar] public int _health = 4;
 }
