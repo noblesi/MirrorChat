8a4ebc813660d51ad2d94d58085abbb51c3d0001
조현승 <danitech171791@gmail.com>
2024-05-30 19:20:19 +0900

    [feat/DB] 데이터베이스 커넥션 테스트

--- Assets/Mirror/Ref/MirrorChat_Danitech/Scripts/DatabaseUI.cs
+++ Assets/Mirror/Ref/MirrorChat_Danitech/Scripts/DatabaseUI.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
+using MySql.Data.MySqlClient;
+using System;
 
 public class DatabaseUI : MonoBehaviour
 {
@@ -8,8 +10,41 @@ public class DatabaseUI : MonoBehaviour
    [SerializeField] Text Text_DBResult;
    [SerializeField] Text Text_Log;
 
+    private bool _connectTestComplete = false;
+
+    [Header("ConnectionInfo")]
+    [SerializeField] string _ip = "127.0.0.1";
+    [SerializeField] string _dbName = "test";
+    [SerializeField] string _uid = "root";
+    [SerializeField] string _pwd = "1234";
+
+    private static MySqlConnection _dbConnection;
+    private bool ConnectTest()
+    {
+        string connectStr = $"Server={_ip};Database={_dbName};Uid={_uid};Pwd={_pwd};";
+
+        try
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectStr))
+            {
+                _dbConnection = conn;
+                conn.Open();
+            }
+
+            Text_Log.text = "DB 연결을 성공했습니다!";
+            return true;
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning($"e: {e.ToString()}");
+            Text_Log.text = "DB 연결 실패!";
+            return false;
+        }
+    }
+
     public void OnClick_TestDBConnect()
     {
+        _connectTestComplete = ConnectTest();
     }
 
     public void OnSubmit_SendQuery()
